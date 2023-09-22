0a4f2f2f14f4754c862858592a021913c1fbe103
Midzuchii <dragonover1@hotmail.co.uk>
2023-09-22 16:22:40 +0100

    More bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,9 @@ intents.message_content = True  # Enable the message content intent
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 bot.start_time = None
+owner_id = 270254006096494592  # Replace with your owner's user ID
+log_channel_id = 1154795053592612895  # Replace with your log channel ID
+waiting_channel_name = "waiting"  # Replace with your waiting channel name
 
 @bot.event
 async def on_ready():
@@ -27,15 +30,15 @@ async def on_ready():
 @bot.command()
 async def register(ctx, unique_id):
     # Check if the user is a moderator or the owner
-    if "Mod" in [role.name for role in ctx.author.roles] or ctx.author.id == 270254006096494592:
+    if "Mod" in [role.name for role in ctx.author.roles] or ctx.author.id == owner_id:
         # Log the registration request
-        log_channel = ctx.guild.get_channel(1154795053592612895)  # Replace with your log channel ID
+        log_channel = bot.get_channel(log_channel_id)
         if log_channel:
             log_message = f"Registration request from {ctx.author.name} (ID: {ctx.author.id}) with unique ID: {unique_id}"
             await log_channel.send(log_message)
 
             # Notify moderators and owner
-            owner = ctx.guild.get_member(270254006096494592)  # Replace with your owner's user ID
+            owner = bot.get_user(owner_id)
             moderators = [member for member in ctx.guild.members if "Mod" in [role.name for role in member.roles]]
             notification_message = f"New registration request from {ctx.author.mention} with unique ID: {unique_id}."
             
@@ -44,7 +47,13 @@ async def register(ctx, unique_id):
             for moderator in moderators:
                 await moderator.send(notification_message)
             
-            await ctx.send("Registration request sent for approval.")
+            # Move the user to the waiting channel
+            waiting_channel = discord.utils.get(ctx.guild.text_channels, name=waiting_channel_name)
+            if waiting_channel:
+                await ctx.author.move_to(waiting_channel)
+                await ctx.send(f"Registration request sent for approval. You are now in the {waiting_channel.mention} channel.")
+            else:
+                await ctx.send("Registration log channel not found. Please set it up.")
         else:
             await ctx.send("Registration log channel not found. Please set it up.")
     else:
@@ -52,7 +61,7 @@ async def register(ctx, unique_id):
 
 @bot.command()
 async def approve(ctx, user: discord.Member):
-    if "Mod" in [role.name for role in ctx.author.roles] or ctx.author.id == 270254006096494592:
+    if "Mod" in [role.name for role in ctx.author.roles] or ctx.author.id == owner_id:
         # Add your approval logic here
         # Log the approval action if needed
         await ctx.send(f"Approved user {user.name}.")
