7d7d1575f107f5eaa0ccd0879b6ad7bc75009071
Midzuchii <dragonover1@hotmail.co.uk>
2023-09-22 18:04:20 +0100

    more fixes

--- bot.py
+++ bot.py
@@ -3,22 +3,20 @@ from discord.ext import commands
 import datetime
 from decouple import config
 
-# Use the `config` function from decouple to read the TOKEN from the .env file
 TOKEN = config('TOKEN')
 PREFIX = "!"
 
-# Define your bot's intents
 intents = discord.Intents.default()
-intents.typing = False  # Disable typing notifications, if desired
-intents.presences = False  # Disable presence updates, if desired
-intents.message_content = True  # Enable the message content intent
+intents.typing = False
+intents.presences = False
+intents.message_content = True
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 bot.start_time = None
-owner_id = 270254006096494592  # Replace with your owner's user ID
-log_channel_id = 1154795053592612895  # Replace with your standard log channel ID
-waiting_channel_id = 1154816815273357384  # Replace with your waiting channel ID
-registration_log_channel_id = 1154811703125627012  # Replace with your registration log channel ID
+owner_id = 270254006096494592
+log_channel_id = 1154795053592612895
+waiting_channel_id = 1154816815273357384
+registration_log_channel_id = 1154811703125627012
 
 # Dictionary to store user registration data
 user_registrations = {}
@@ -27,11 +25,8 @@ user_registrations = {}
 async def on_ready():
     bot.start_time = datetime.datetime.utcnow()
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
-
-    # Set the bot's status to "online"
     await bot.change_presence(status=discord.Status.online)
-
-    owner = await bot.fetch_user(owner_id)  # Fetch the owner's user object
+    owner = await bot.fetch_user(owner_id)
     if owner:
         notification_message = "Bot is now online and ready."
         await owner.send(notification_message)
@@ -40,31 +35,21 @@ async def on_ready():
 
 @bot.command()
 async def register(ctx, unique_id):
-    # Check if the user is a moderator or the owner
     if "Mod" in [role.name for role in ctx.author.roles] or ctx.author.id == owner_id:
-        # Log the registration request in the registration log channel
         registration_log_channel = bot.get_channel(registration_log_channel_id)
         if registration_log_channel:
             log_message = f"Registration request from {ctx.author.name} (ID: {ctx.author.id}) with unique ID: {unique_id}"
             await registration_log_channel.send(log_message)
-
-            # Notify moderators and owner
-            owner = await bot.fetch_user(owner_id)  # Fetch the owner's user object
+            owner = await bot.fetch_user(owner_id)
             moderators = [member for member in ctx.guild.members if "Mod" in [role.name for role in member.roles]]
             notification_message = f"New registration request from {ctx.author.mention} with unique ID: {unique_id}."
-
-            # Send notifications
             await owner.send(notification_message)
             for moderator in moderators:
                 await moderator.send(notification_message)
-
-            # Store user registration data
             user_registrations[ctx.author.id] = unique_id
-
-            # Move the user to the waiting channel (text channel)
             waiting_channel = bot.get_channel(waiting_channel_id)
             if waiting_channel:
-                await ctx.author.move_to(None)  # Move to the text channel, not a voice channel
+                await ctx.author.move_to(None)
                 await ctx.send(f"Registration request sent for approval. You are now in the {waiting_channel.mention} channel.")
             else:
                 await ctx.send("Waiting channel not found. Please set it up.")
@@ -76,22 +61,15 @@ async def register(ctx, unique_id):
 @bot.command()
 async def approve(ctx, user: discord.Member):
     if "Mod" in [role.name for role in ctx.author.roles] or ctx.author.id == owner_id:
-        # Check if the user has a registration
         if user.id in user_registrations:
             unique_id = user_registrations[user.id]
-
-            # Add your approval logic here
-            # Log the approval action in the standard log channel
             log_channel = bot.get_channel(log_channel_id)
             if log_channel:
                 log_message = f"Approved user {user.name}. Unique ID: {unique_id}"
                 await log_channel.send(log_message)
             else:
                 print("Standard log channel not found. Please set it up.")
-
-            # Remove the user's registration data
             del user_registrations[user.id]
-
             await ctx.send(log_message)
         else:
             await ctx.send("This user doesn't have a pending registration.")
@@ -100,14 +78,12 @@ async def approve(ctx, user: discord.Member):
 
 @bot.command()
 async def feedback(ctx, *, message):
-    # Log the feedback message in the standard log channel
     log_channel = bot.get_channel(log_channel_id)
     if log_channel:
         log_message = f"Feedback received from {ctx.author.name}: {message}"
         await log_channel.send(log_message)
     else:
         print("Standard log channel not found. Please set it up.")
-
     await ctx.send("Thank you for your feedback!")
 
 @bot.command()
@@ -121,7 +97,6 @@ async def uptime(ctx):
 
 @bot.command()
 async def shelp(ctx):
-    # Provide information about available commands
     help_message = (
         "Available commands:\n"
         "!register [unique_id] - Register a user with a unique ID (for moderators and owner).\n"
@@ -130,5 +105,29 @@ async def shelp(ctx):
         "!uptime - Display bot uptime.\n"
     )
     await ctx.send(help_message)
+
+@bot.command()
+async def mute(ctx, member: discord.Member, duration=None, *, reason=None):
+    if ctx.author.guild_permissions.manage_roles:
+        mute_role = discord.utils.get(ctx.guild.roles, name="Silly Snail")
+        if mute_role:
+            await member.add_roles(mute_role, reason=reason)
+            await ctx.send(f'Muted {member.mention} for reason: {reason}')
+        else:
+            await ctx.send("The 'Silly Snail' mute role does not exist.")
+    else:
+        await ctx.send("You don't have permission to manage roles.")
+
+@bot.command()
+async def unmute(ctx, member: discord.Member):
+    if ctx.author.guild_permissions.manage_roles:
+        mute_role = discord.utils.get(ctx.guild.roles, name="Silly Snail")
+        if mute_role:
+            await member.remove_roles(mute_role)
+            await ctx.send(f'Unmuted {member.mention}')
+        else:
+            await ctx.send("The 'Silly Snail' mute role does not exist.")
+    else:
+        await ctx.send("You don't have permission to manage roles.")
 
 bot.run(TOKEN)
